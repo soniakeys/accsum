a6583f4f9202003d29dc0c727a2aeb5a04e68162
Sonia Keys <skeys.cfa.harvard.edu>
2014-12-23 23:34:07 +0000

    update

--- doc.go
+++ doc.go
@@ -3,10 +3,9 @@
 
 // Accsum contains routines for accurate floating point arithmetic.
 //
-// These are implementation of algorithms described in "Accurate floating point
-// summation" by S. Rump, T. Ogita, and S. Oishi.
-//
-//  http://www.ti3.tu-harburg.de/rump/
-//  http://www.ti3.tu-harburg.de/paper/rump/RuOgOi07I.pdf
-//  http://www.ti3.tu-harburg.de/paper/rump/RuOgOi07II.pdf
+// These are mostly implementations of algorithms developed by Siegfried M.
+// Rump.  See http://www.ti3.tu-harburg.de/rump/Research/topics.php#dot_product
+// for an overview and links to published papers.  See also
+// http://www.ti3.tu-harburg.de/rump/AccSum.zip for reference versions of these
+// algorithms in Matlab.
 package accsum
