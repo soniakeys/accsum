9f5a8ae0809fc77c757ea725eb9630e728a11cba
Sonia Keys <soniakeys@gmail.com>
2015-01-03 16:09:14 -0500

    examples in godoc order

--- sum_test.go
+++ sum_test.go
@@ -9,12 +9,6 @@ import (
 	"github.com/soniakeys/accsum"
 )
 
-func ExampleCondSum() {
-	p := []float64{1e100, 1, -1e100}
-	fmt.Println(accsum.CondSum(accsum.Sum2, p))
-	// Output: 2e+100
-}
-
 func ExampleCondDot() {
 	x := []float64{1e10, 1, 1e10}
 	y := []float64{1e10, 1, -1e10}
@@ -22,26 +16,10 @@ func ExampleCondDot() {
 	// Output: 4e+20
 }
 
-func ExampleSum() {
-	p := []float64{1, 2, 3, 4}
-	fmt.Println(accsum.Sum(p))
-	// Output: 10
-}
-
-func ExampleXSum() {
-	n := 54321
-	p := make([]float64, n+1)
-	for i := range p {
-		p[i] = float64(i)
-	}
-	p[0] = 1e20
-	fmt.Printf("Simple:   %.16e\n", accsum.Sum(p))
-	fmt.Printf("XSum:     %.16e\n", accsum.XSum(p))
-	fmt.Println("Triangle:            ", n*(n+1)/2)
-	// Output:
-	// Simple:   1.0000000000146203e+20
-	// XSum:     1.0000000000147541e+20
-	// Triangle:             1475412681
+func ExampleCondSum() {
+	p := []float64{1e100, 1, -1e100}
+	fmt.Println(accsum.CondSum(accsum.Sum2, p))
+	// Output: 2e+100
 }
 
 func ExampleDot() {
@@ -49,22 +27,6 @@ func ExampleDot() {
 	y := []float64{3, 1, 4}
 	fmt.Println(accsum.Dot(x, y))
 	// Output: 17
-}
-
-func ExampleXDot() {
-	n := 4321
-	x := make([]float64, n+1)
-	for i := range x {
-		x[i] = float64(i)
-	}
-	x[0] = 1e11
-	fmt.Printf("Simple:   %.16e\n", accsum.Dot(x, x))
-	fmt.Printf("XDot:     %.16e\n", accsum.XDot(x, x))
-	fmt.Println("Square triangle:      ", n*(n+1)*(2*n+1)/6)
-	// Output:
-	// Simple:   1.0000000000026734e+22
-	// XDot:     1.0000000000026902e+22
-	// Square triangle:       26901858961
 }
 
 func ExampleKahanSum() {
@@ -113,4 +75,42 @@ func ExamplePriestSum() {
 	// Simple:     1.0000000000146203e+20
 	// PriestSum:  1.0000000000147541e+20
 	// Triangle:               1475412681
+}
+
+func ExampleSum() {
+	p := []float64{1, 2, 3, 4}
+	fmt.Println(accsum.Sum(p))
+	// Output: 10
+}
+
+func ExampleXDot() {
+	n := 4321
+	x := make([]float64, n+1)
+	for i := range x {
+		x[i] = float64(i)
+	}
+	x[0] = 1e11
+	fmt.Printf("Simple:   %.16e\n", accsum.Dot(x, x))
+	fmt.Printf("XDot:     %.16e\n", accsum.XDot(x, x))
+	fmt.Println("Square triangle:      ", n*(n+1)*(2*n+1)/6)
+	// Output:
+	// Simple:   1.0000000000026734e+22
+	// XDot:     1.0000000000026902e+22
+	// Square triangle:       26901858961
+}
+
+func ExampleXSum() {
+	n := 54321
+	p := make([]float64, n+1)
+	for i := range p {
+		p[i] = float64(i)
+	}
+	p[0] = 1e20
+	fmt.Printf("Simple:   %.16e\n", accsum.Sum(p))
+	fmt.Printf("XSum:     %.16e\n", accsum.XSum(p))
+	fmt.Println("Triangle:            ", n*(n+1)/2)
+	// Output:
+	// Simple:   1.0000000000146203e+20
+	// XSum:     1.0000000000147541e+20
+	// Triangle:             1475412681
 }
